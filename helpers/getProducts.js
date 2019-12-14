ffcfc8ca3912ddbcfac5c4c5ef47214bac4cdee0
Alexander Barna <influsion@gmail.com>
2019-12-14 19:23:08 +0200

    added search

--- helpers/getProducts.js
+++ helpers/getProducts.js
@@ -13,60 +13,95 @@ const { isKey } = require('./isKey');
 const getProducts = function(productArr, params = {}) {
     params = { ...params };
     const keys = Object.keys(params);
+    const isSearch = keys.includes('name');
 
-    return productArr.filter(elem => {
-        elem = { ...elem };
+    const productFiltering = () => {
+        return productArr.filter(elem => {
+            elem = { ...elem };
 
-        const eachParam = key => {
-            const is = isKey(key);
-            const includedKey = is.id || is.name || is.price || is.category || is.subcategory || is.brand || is.origin || is.popular || is.new;
+            const eachParam = key => {
+                const is = isKey(key);
+                const includedKey = is.id || is.name || is.price || is.category || is.subcategory || is.brand || is.origin || is.popular || is.new;
 
-            if (!includedKey) {
-                return;
-            }
+                if (!includedKey) {
+                    return;
+                }
 
-            const multipleKey = is.id || is.price || is.category || is.subcategory || is.brand || is.origin;
-            const numericalKey = is.id || is.price;
-            const searchKey = is.name;
+                const multipleKey = is.id || is.price || is.category || is.subcategory || is.brand || is.origin;
+                const numericalKey = is.id || is.price;
+                const searchQuery = is.name;
 
-            // Transformation
-            if (multipleKey) {
-                params[key] = [ params[key] ].flat();
-            }
+                // Transformation
+                if (multipleKey) {
+                    params[key] = [ params[key] ].flat();
+                }
 
-            // Transformation
-            if (numericalKey) {
-                params[key] = params[key].map(item => parseInt(item));
-                elem[key] = parseInt(elem[key]);
-            }
+                // Transformation
+                if (numericalKey) {
+                    params[key] = params[key].map(item => parseInt(item));
+                    elem[key] = parseInt(elem[key]);
+                }
 
-            // Comparison
-            if (multipleKey && !is.price) {
-                return params[key].includes(elem[key]);
-            }
+                // Comparison
+                if (multipleKey && !is.price) {
+                    return params[key].includes(elem[key]);
+                }
 
-            // Comparison
-            if (is.price) {
-                const price = elem[key];
-                const [ min, max ] = params[key];
+                // Comparison
+                if (is.price) {
+                    const price = elem[key];
+                    const [ min, max ] = params[key];
 
-                return min <= price && price <= max;
-            }
+                    return min <= price && price <= max;
+                }
 
-            if (is.popular || is.new) {
-                return `${params[key]}`.toLowerCase() === `${elem[key]}`.toLowerCase();
-            }
+                if (is.popular || is.new) {
+                    return `${params[key]}`.toLowerCase() === `${elem[key]}`.toLowerCase();
+                }
 
-            // Comparison
-            if (searchKey) {
-                //
-            }
+                // Comparison
+                if (searchQuery) {
+                    //
+                }
+            };
+
+            return keys
+                .map(eachParam)
+                .every(elem => elem === true);
+        });
+    };
+
+    const productSearch = () => {
+        const { quantityOfReturned = null, name: searchQuerySourse } = params;
+        let searchQuery = searchQuerySourse.trim().replace(/\s+/, ' ');
+        const re = new RegExp(searchQuery, 'gi');
+
+        const isCorrectSearchQuery = true;
+
+        const processSearchQuery = () => {
+            return productArr
+                .map(elem => {
+                    const { name } = elem;
+
+                    return {
+                        searchQuery,
+                        searchQueryPosition: name.search(re),
+                        product: elem,
+                    }
+                })
+                .filter(({ searchQueryPosition }) => +searchQueryPosition !== -1)
+                .sort((a, b) => a.searchQueryPosition - b.searchQueryPosition)
+                .slice(0, quantityOfReturned ? quantityOfReturned : undefined)
+                .map(({ product }) => {
+                    product.searchedName = product.name.replace(re, `<mark>${searchQuery}</mark>`);
+                    return product;
+                });
         };
 
-        return keys
-            .map(eachParam)
-            .every(elem => elem === true);
-    });
+        return isCorrectSearchQuery ? processSearchQuery() : [];
+    };
+
+    return isSearch ? productSearch() : productFiltering() ;
 }
 
 module.exports = { getProducts };
